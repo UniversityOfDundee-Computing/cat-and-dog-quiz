0a6c3826a4ec5e5cc2d3ecb15df0fa3a96687e94
SandSide <37347875+SandSide@users.noreply.github.com>
2023-12-04 04:40:52 +0000

    feat: get random cat info

--- src/script.js
+++ src/script.js
@@ -1,17 +1,18 @@
+const apiKey = 'live_Y2GmDHCqmM2Nz3Jxg7Yv1GeLQywtvYFHgjr9haAIhv0kyq9dRelEbF6VDXattX7m';
+
+const breed = {
+    id: "",
+    name: ""
+}
+
+
 function getCatBreeds(){
-
-    var breeds = []
-
-    fetch('https://api.thecatapi.com/v1/breeds')
+    return fetch('https://api.thecatapi.com/v1/breeds')
     .then(response => response.json())
     .then(data => {
-
-        data.forEach(element => {
-            breeds.push(element.name)
-        });
+        var breeds = data.map(element => ({id: element.id, name: element.name}));
+        return breeds;
     })
-
-    return breeds
 }
 
 function getQuestion(){
@@ -26,7 +27,28 @@ function getQuestion(){
     })
 }
 
+function getCat(breeds){
+
+    var randomBreed = breeds[Math.floor(Math.random() * breeds.length)];
+
+    fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${randomBreed.id}&api_key=${apiKey}`)
+    .then(response => response.json())
+    .then(data =>{
+        console.log(data);
+    })
+    .catch(err =>{
+        console.log(err);
+    })
+}
+
 window.onload = function(){
 
-    var carBreeds = getCatBreeds();
+    var catBreeds = getCatBreeds();
+
+    getCatBreeds()
+    .then(catBreeds => {
+        console.log(catBreeds)
+        getCat(catBreeds);
+    });
+
 }
