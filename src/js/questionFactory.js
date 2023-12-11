a6895a173300c3cfec97df861ae0fc9e9328524c
SandSide <37347875+SandSide@users.noreply.github.com>
2023-12-11 23:33:00 +0000

    feat: create dog and cat multiple choice breed questions

--- src/js/questionFactory.js
+++ src/js/questionFactory.js
@@ -20,4 +20,32 @@ class QuestionFactory{
 
         return questions;
     }
+
+    static createCatDogMultipleChoiceBreedQuestions(catBreeds, dogBreeds, amount){
+
+        var questions = []
+
+        for (let i = 0; i < amount; i++) {
+
+            var newQuestion;
+
+            if (i % 2 === 0) {
+                var randomBreed = catBreeds[Math.floor(Math.random() * catBreeds.length)];
+                var possibleAnswers = getMultipleUniqueBreeds(catBreeds, 4, randomBreed.name);
+
+                newQuestion = new MultipleChoiceQuestion('What is the Breed?', randomBreed.name, possibleAnswers, 'cat', randomBreed.id);
+            }
+            else {
+                var randomBreed = dogBreeds[Math.floor(Math.random() * dogBreeds.length)];
+                var possibleAnswers = getMultipleUniqueBreeds(dogBreeds, 4, randomBreed.name);
+
+                newQuestion = new MultipleChoiceQuestion('What is the Breed?', randomBreed.name, possibleAnswers, 'dog', randomBreed.id);
+            }
+
+            questions.push(newQuestion);
+        }
+
+        return questions;
+
+    }
 }
