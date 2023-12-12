6138ee8b71f0011b8316186032efe109f10b9c02
SandSide <37347875+SandSide@users.noreply.github.com>
2023-12-12 19:02:15 +0000

    chore: comment api.js

--- src/js/api.js
+++ src/js/api.js
@@ -4,6 +4,12 @@ const dogApiKey = 'live_i7mLqdlkY1JrPjPa2AgThy3OXU8jmZQLYMW74Pu2I4Fvl126MqbbLtfx
 var catBreeds;
 var dogBreeds;
 
+/**
+ * Get breeds of an anima using an API
+ * 
+ * @param {string} animal - Name of the name to get breeds of
+ * @returns 
+ */
 async function getBreeds(animal) {
     return fetch(`https://api.the${animal}api.com/v1/breeds`)
         .then(response => response.json())
@@ -12,8 +18,16 @@ async function getBreeds(animal) {
         })
 }
 
+/**
+ * Fetch details of an animal (with a picture), based on breed
+ * 
+ * @param {string} breed - Breed on animal to get details of
+ * @param {string} type - Type of animal to get details of
+ * @returns 
+ */
 function getAnimalDetails(breed, type) {
 
+    // Determine which api to call
     var api = type == 'dog' ? dogApiKey : catApiKey;
 
     return fetch(`https://api.the${type}api.com/v1/images/search?breed_ids=${breed}&api_key=${api}`)
