ab76e6033b7185df1afe0ac0888e8930b097579c
SandSide <37347875+SandSide@users.noreply.github.com>
2023-12-12 19:14:39 +0000

    docs: comment fact.js

--- src/js/fact.js
+++ src/js/fact.js
@@ -1,3 +1,10 @@
+
+/**
+ * Try to get a fact about an animal
+ * 
+ * @param {string} animalType - Animal type to get a fact about
+ * @returns {string} - Fact about the animal
+ */
 async function getFact(animalType) {
 
     const maxRetries = 15;
@@ -5,11 +12,13 @@ async function getFact(animalType) {
     var retries = 0;
     var foundValidFact = false;
 
+    // Try to fetch fact
     while (!foundValidFact && retries <= maxRetries) {
 
         const response = await fetch(`https://cat-fact.herokuapp.com/facts/random?animal_type=${animalType}`);
         const data = await response.json();
 
+        // If the fact is suitable to display
         if (data.status.verified === true) {
             return data.text;
         }
@@ -23,27 +32,38 @@ async function getFact(animalType) {
 
 }
 
-function displayFact(animalType, callback){
+/**
+ * Display fact
+ * 
+ * @param {string} animalType - Animal to display fact about
+ * @param {*} callback - Function to attach to fact popup button
+ */
+function displayFact(animalType, callback) {
 
-        // Show fact
-        getFact(animalType)
+    // Show fact
+    getFact(animalType)
         .then(fact => updateFact(fact, () => {
             callback();
         }))
         .catch(err => callback());
-
 }
 
-function updateFact(fact, callback){
+/**
+ * Update fact popup element with fact
+ * 
+ * @param {string} fact - Fact to display
+ * @param {*} callback - Action added to buttons event listener
+ */
+function updateFact(fact, callback) {
 
-    console.log('Display Fact');
+    console.log('Displaying Fact');
 
     // Show fact pop up
     document.getElementById('fact').classList.remove('hidden');
 
     document.getElementById('fact-text').textContent = fact;
 
-    // Remove prev buton due to ahvign an event listiner
+    // Remove previous button due to having an event listener
     document.getElementById('next-question-btn').remove();
 
     // Create button to go to next question
@@ -53,7 +73,8 @@ function updateFact(fact, callback){
     btn.innerHTML = 'Next';
     btn.id = 'next-question-btn';
 
+    // Call callback on click
     btn.addEventListener('click', callback);
-    
+
     document.getElementById('fact-btns').appendChild(btn);
 }
